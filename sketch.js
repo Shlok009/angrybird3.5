7d2ec1a82a00a567f490cfa6a8a4199b882a3cc2
Shlok <shlok.patil.25@srsgnyandham.org>
2021-05-21 18:44:33 +0530

    made bird to stretch and fly

--- sketch.js
+++ sketch.js
@@ -38,10 +38,16 @@ function setup(){
 
     bird = new Bird(100,100);
 
-    log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,log6.body);
+    //log6 = new Log(230,180,80, PI/2);
+    slingShot = new Slingshot(bird.body,{x:300,y:100});
+    
 }
-
+function mouseDragged(){
+Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY})
+}
+function mouseReleased(){
+slingShot.fly()    
+}
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
@@ -63,6 +69,6 @@ function draw(){
 
     bird.display();
     platform.display();
-    log6.display();
-    chain.display();    
+    //log6.display();
+    slingShot.display();    
 }
